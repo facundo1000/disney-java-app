a047708b791ae51a30ff5e6b1fea4e964a32cc93
Mr.Fish <69169769+facundo1000@users.noreply.github.com>
2022-03-27 08:31:15 -0300

    build: start of the proyect

---
+++ service-characters/src/main/java/com/disney/servicecharacters/model/Character.java
@@ -0,0 +1,12 @@
+package com.disney.servicecharacters.model;
+
+import lombok.Data;
+
+
+public class Character {
+    private String image;
+    private String name;
+    private String age;
+    private String weight
+
+}
---
+++ service-characters/src/test/java/com/disney/servicecharacters/ServiceCharactersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.disney.servicecharacters;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceCharactersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
